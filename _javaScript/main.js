618de43653b5db459cb42ee6783a45badc474071
daniel <danielpereiracamposjunior@gmail.com>
2022-04-27 13:05:44 -0300

    Update main.js
    Final aula 01 inicio aula 02

--- _javaScript/main.js
+++ _javaScript/main.js
@@ -1,10 +1,15 @@
-var nome = "Daniel Campos ";
-var idade = 46;
-var idade2 = 39;
-var frase="Japão é o melhor time do mundo. ";
+//  aula 01 introdução ao  JavaScript
+
+//var nome = "Daniel Campos ";
+//var idade = 46;
+//var idade2 = 39;
+//var frase="Japão é o melhor time do mundo. ";
 //alert(nome + " tem " + idade + " anos");
 //alert(idade + idade2)
 
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase.replace("Japão ", "Brasil "));
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão ", "Brasil "));
+
+//  Aula dois
+

cb9a9cfc8e0ebe769b2ed1b23dd74b0627ca3e4c
daniel <danielpereiracamposjunior@gmail.com>
2022-04-27 11:44:30 -0300

    aula JS Dio
    teste do  console.log

--- _javaScript/main.js
+++ _javaScript/main.js
@@ -1,1 +1,10 @@
-lert("Meu primeiro JS");
+var nome = "Daniel Campos ";
+var idade = 46;
+var idade2 = 39;
+var frase="Japão é o melhor time do mundo. ";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2)
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão ", "Brasil "));
